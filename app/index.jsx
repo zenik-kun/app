d08e41783c5e663e40d0104a7385c57528b3be65
zenik-kun <nikirukyun@gmail.com>
2024-06-19 19:38:37 +0530

    Custom Button Component

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,7 @@ import { ScrollView, Text, View, Image } from 'react-native';
 import { Link } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
 
 export default function App() {
   return (
@@ -33,6 +34,12 @@ export default function App() {
             />
           </View>
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
+
+          <CustomButton 
+            title = "Continue with Email"
+            handlePress = {() => {}}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
