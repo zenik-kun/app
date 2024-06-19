3cd43408b68ac627b855961451fe2d0ad5522f88
zenik-kun <nikirukyun@gmail.com>
2024-06-19 19:45:48 +0530

    router and redirect options on onboarding screen

--- app/index.jsx
+++ app/index.jsx
@@ -37,7 +37,7 @@ export default function App() {
 
           <CustomButton 
             title = "Continue with Email"
-            handlePress = {() => {}}
+            handlePress = {() => router.push('/sign-in')}
             containerStyles="w-full mt-7"
           />
         </View>
