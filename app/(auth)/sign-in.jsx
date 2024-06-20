61ad8db1ecf510789e0f56d846824037f6ce7242
zenik-kun <nikirukyun@gmail.com>
2024-06-20 11:52:36 +0530

    (auth) initial

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants'
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center items-center h-full px-4 my-6">
+          <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]"/>
+
+          <Text className="text-2xl text-white font-psemibold mt-10">Log in to Aora</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
