177bbaf72a892f0a8b6c5a6c6937dc125b980afd
zenik-kun <nikirukyun@gmail.com>
2024-06-21 22:23:28 +0530

    global provider done

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -1,45 +1,46 @@
-import { createContext, useContext, useState, useEffect, Children } from "react";
-import { getCurrentUser } from "../lib/appwrite"; 
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { getCurrentUser } from "../lib/appwrite";
 
 const GlobalContext = createContext();
 export const useGlobalContext = () => useContext(GlobalContext);
+
 const GlobalProvider = ({ children }) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [user, setUser] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+  const [isLogged, setIsLogged] = useState(false);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        getCurrentUser()
-            .then((res) => {
-                if (res) {
-                    setIsLoading(true);
-                    setUser(res)
-                } else {
-                    setIsLoggedIn(false)
-                    setUser(null)
-                }
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-            .finally(() => {
-                setIsLoading(false)
-            })
-    }, []);
+  useEffect(() => {
+    getCurrentUser()
+      .then((res) => {
+        if (res) {
+          setIsLogged(true);
+          setUser(res);
+        } else {
+          setIsLogged(false);
+          setUser(null);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
-    return (
-        <GlobalContext.Provider
-            value = {{
-                isLoggedIn,
-                setIsLoggedIn,
-                user,
-                setUser,
-                isLoading
-            }}
-        >
-            {children}
-        </GlobalContext.Provider>
-    )
-}
+  return (
+    <GlobalContext.Provider
+      value={{
+        isLogged,
+        setIsLogged,
+        user,
+        setUser,
+        loading,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
 
 export default GlobalProvider;
